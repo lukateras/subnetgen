3c4c6be79fef3c238d24f64ed32cff661ddc5b11
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-01-03 19:41:37 +0300

    Bump Nixpkgs to 201d739b0ffbebceb444864d1856babcd1a666a8

--- pkgs.nix
+++ pkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/7df10f388dabe9af3320fe91dd715fc84f4c7e8a.tar.gz";
-  sha256 = "14n9nwdmd1jvvic1rnyw4023fm97b0xn4nq801l29vpnfzyab04w";
+  url = "https://github.com/NixOS/nixpkgs/archive/201d739b0ffbebceb444864d1856babcd1a666a8.tar.gz";
+  sha256 = "0mfkzmylglpw84w85zs3djpspcx45bg3s62hk4j44dxl2p0fvggj";
 })
